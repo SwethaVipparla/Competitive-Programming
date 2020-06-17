05fcc87d6e4880ff4d5e77ad3f24f17809f11bac
Swetha Vipparla <65361205+SwethaVipparla@users.noreply.github.com>
2020-06-17 17:56:58 +0400

    Create newalgorithm.cpp

---
+++ newalgorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n, a, d;
+	cin >> n >> a;
+	while(a--)
+	{
+	    d = n % 10;
+	    if(d == 0)
+	        n /= 10;
+	    else
+	        n -= 1;
+	    if(n == 0)
+	        break;
+	}
+	cout << n << "\n";
+	return 0;
+}

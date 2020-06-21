3aec85061afd2d2b685f4646907ba28fa6580da3
Swetha Vipparla <65361205+SwethaVipparla@users.noreply.github.com>
2020-06-21 21:57:06 +0400

    Solution for FACPOW

--- factorsandpowers.cpp
+++ factorsandpowers.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 void solve()
 {
-    ll x , k, sum = 0, sum2 = 0, i;
-	cin >> x >> k;
-	for(i = 2;i <= x; i++)
-	{
-	    {
-	        if(x % i == 0)
-	            sum += pow(i, k);
-	    }
-	}
-	cout << sum << " ";
-	for(i = 2; i < x; i++)
-	{
-	    if(k % i == 0)
-	        sum2 += x * i;
-	}
-	cout << sum2;
+     ll x , k, sum = 0, sum2 = 0, i;
+     cin >> x >> k;
+     for(i = 2;i <= x; i++)
+     {
+	  {
+	      if(x % i == 0)
+	          sum += pow(i, k);
+	   }
+     }
+     cout << sum << " ";
+     for(i = 2; i < x; i++)
+     {
+	  if(k % i == 0)
+	      sum2 += x * i;
+     }
+     cout << sum2;
 }
 
 int main() 
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	ll t;
-	cin >> t;
-	while(t--)
-	{
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     ll t;
+     cin >> t;
+     while(t--)
+     {
         solve();
         cout << "\n";
-    }
-	return 0;
+     }
+     return 0;
 }

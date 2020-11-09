3c944530494e42af372b82254fec55bf69943985
Swetha Vipparla <65361205+SwethaVipparla@users.noreply.github.com>
2020-11-09 10:32:40 +0400

    Solution for 1444A

---
+++ division.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int T, x, y;
+
+signed main()
+{
+	scanf("%lld", &T);
+	while (T--)
+	{
+		scanf("%lld %lld", &x, &y);
+		if (x%y != 0)
+		    printf("%lld\n", x);
+		else 
+		{
+			int ans=0,t=y;
+			for (int i=2;i*i<=y;i++)
+			if (y%i==0)
+			{
+				while (y%i==0)
+				    y /= i;
+				int a=x;
+				while (a%t == 0)
+				    a /= i;
+				ans=max(ans,a);
+			}
+			if (y!=1)
+			{
+				int a=x;
+				while (a%t == 0)
+				    a/=y;
+				ans=max(ans,a);
+			}
+			printf("%lld\n",ans);
+		}
+	}
+}
